a4dd8b0f93de50235f39499164a70c67aeb2b2c6
Mushfiqur Niaz <mushfiqurniaz33@gmail.com>
2024-08-18 14:26:13 +0600

    fixed issue

--- server/index.js
+++ server/index.js
@@ -5,11 +5,14 @@ const cors = require("cors");
 const DB_Server_Connection = require("./db/dbConn");
 const app = express();
 app.use(cors({
-  origin: "https://mern-to-do-app-client.vercel.app", 
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+  origin: "https://mern-to-do-app-client.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.options('*', cors());
+
 app.use(express.json());
 const router = require("./routes/routes");
 
